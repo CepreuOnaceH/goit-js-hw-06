d74b4256f5b500659d640329305d16ca158fd1bd
CepreuOnaceH <ykcyc13@gmail.com>
2023-07-17 21:07:27 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,28 @@
+const divControls = document.getElementById("controls");
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]");  
+const divBox = document.getElementById("boxes");
+
+createButton.addEventListener("click", function () {
+  const amount = document.querySelector("input").value;
+  createBoxes(amount);
+})
+destroyButton.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  let boxSize = 30;
+  let boxesHTML = "";
+  for (let i = 0; i < amount; i+=1) {
+    let size = boxSize + i * 10;
+    let color = getRandomHexColor();
+    let box = `<div style="width: ${size}px; height: ${size}px; background-color: ${color}"></div>`;
+    boxesHTML += box;
+  }
+  divBox.innerHTML = boxesHTML;
+}
+function destroyBoxes() {
+  divBox.innerHTML = "";
+}
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
